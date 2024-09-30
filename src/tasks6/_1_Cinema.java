e72c91dcd7c73c66c6755d552e519ab9cb6a6290
Dimitar Elenkov <dim_elenkov@abv.bg>
2024-09-30 11:55:30 +0300

    second commit

---
+++ src/tasks6/_1_Cinema.java
@@ -0,0 +1,28 @@
+package tasks6;
+
+import java.util.Scanner;
+
+public class _1_Cinema {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String projection = scanner.nextLine();
+        int rows = Integer.parseInt(scanner.nextLine());
+        int columns = Integer.parseInt(scanner.nextLine());
+
+        double income = 0;
+
+        if (projection.equals("Premiere")) {
+            income = rows * columns * 12;
+        }
+        else if (projection.equals("Normal")) {
+            income = rows * columns * 7.50;
+        }
+        else if (projection.equals("Discount")) {
+            income = rows * columns * 5;
+        }
+
+        System.out.printf("%.2f leva", income);
+
+
+    }
+}
